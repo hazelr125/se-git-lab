dfd80fb41680f08e663437620cb21f58ff3f17ce
Hazel Ratna <hazelratna125@gmail.com>
2026-03-30 19:20:08 +0530

    Added database connection method

--- DataProcessor.java
+++ DataProcessor.java
@@ -2,5 +2,10 @@
 public class DataProcessor {
     public static void main(String[] args) {
         System.out.println("Backend system initialized.");
+        connectDatabase();
+    }
+
+    public static void connectDatabase() {
+        System.out.println("Connecting to the database... Success!");
     }
 }
